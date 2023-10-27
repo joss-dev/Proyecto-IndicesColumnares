ce07de6bb764c047d061f2b57b24229ee38afebc
Jmjoss <josekandia42@gmail.com>
2023-10-26 21:42:15 -0300

    add sql

---
+++ IndicesColumnares.sql
@@ -0,0 +1,64 @@
+-- Variables para controlar el bucle de inserción
+DECLARE @RowCount INT = 0;
+DECLARE @TotalRows INT = 900000; -- Número total de registros a insertar
+
+-- Habilitar IDENTITY_INSERT si es necesario
+-- SET IDENTITY_INSERT GASTO ON;
+
+-- Inicio del bucle de inserción
+WHILE @RowCount < @TotalRows
+BEGIN
+    -- Generar datos aleatorios
+    DECLARE @idprovincia INT;
+    DECLARE @idlocalidad INT;
+    DECLARE @idconsorcio INT;
+    
+    -- valores aleatorios para idprovincia, idlocalidad e idconsorcio de la tabla "consorcio"
+    SELECT TOP 1 @idprovincia = idprovincia, @idlocalidad = idlocalidad, @idconsorcio = idconsorcio
+    FROM consorcio
+    ORDER BY NEWID();							--genera valores entre 1 y 24
+    
+								-- aleatorio entre 1 y 9
+    DECLARE @periodo INT = FLOOR(RAND() * 9) + 1;; 
+								-- Fecha en los últimos 365 días
+    DECLARE @fechapago DATETIME = DATEADD(DAY, -CAST((RAND() * 365) AS INT), GETDATE()); 
+								--genera valores entre 1 y 5
+    DECLARE @idtipogasto INT = FLOOR(RAND() * 5) + 1;
+								--genera valores entre 1 y 10
+    
+								-- Importe aleatorio
+    DECLARE @importe DECIMAL(8, 2) = CAST(RAND() * 1000 AS DECIMAL(8, 2)); 
+
+    -- Insertar el registro aleatorio en la tabla GASTO
+    INSERT INTO gasto (idprovincia, idlocalidad, idconsorcio,periodo, fechapago, idtipogasto, importe)
+    VALUES (@idprovincia, @idlocalidad,@idconsorcio ,@periodo, @fechapago, @idtipogasto, @importe);
+
+    -- Incrementar el contador
+    SET @RowCount = @RowCount + 1;
+END
+
+
+--crea un indice agrupado
+--CREATE NONCLUSTERED COLUMNSTORE INDEX NombreDelIndice
+--ON NombreDeLaTabla;
+
+CREATE COLUMNSTORE INDEX i_fechapago ON GASTONEW (fechapago);
+
+
+--índice no agrupado
+--CREATE NONCLUSTERED COLUMNSTORE INDEX NombreDelIndice
+--ON NombreDeLaTabla;
+
+
+SELECT *
+FROM gastonew
+WHERE fechapago = '2017-04-05 00:00:00.000' AND importe = 1980.69
+
+
+SELECT *
+FROM gasto
+WHERE fechapago = '2017-04-05 00:00:00.000' AND importe = 1980.69
+
+SELECT idconsorcio AS AliasColumna
+FROM consorcio
+ORDER BY AliasColumna ASC;
---
+++ Modelo.sql
@@ -0,0 +1,100 @@
+CREATE DATABASE base_consorcio;
+
+USE base_consorcio;
+
+Create table provincia (
+	idprovincia int primary key not null, 
+	descripcion varchar(50) not null,
+	km2 int not null,
+	cantdptos int not null,
+	poblacion int not null,
+	nomcabe varchar(50) not null
+)
+
+Create table localidad (
+	idprovincia int not null, 
+	idlocalidad int not null, 
+	descripcion varchar(50) not null,
+	Constraint PK_localidad PRIMARY KEY (idprovincia, idlocalidad),
+	Constraint FK_localidad_pcia FOREIGN KEY (idprovincia)  REFERENCES provincia(idprovincia)						 					     					     					     				     					     
+)
+
+
+Create table zona (
+	idzona int identity primary key not null, 
+	descripcion varchar(50) not null
+)
+
+
+Create table conserje (
+	idconserje int identity primary key not null, 
+	apeynom varchar(50) not null,
+	tel varchar(20) not null,
+	fechnac datetime not null,
+	estciv varchar(1)  NULL default ('S')
+	CONSTRAINT CK_estadocivil CHECK (estciv IN ('S', 'C','D','O'))
+)
+
+
+Create table administrador	(
+	idadmin int identity primary key not null, 
+	apeynom varchar(50) not null,
+	viveahi varchar(1)  NULL default ('N') 
+	CONSTRAINT CK_habitante_viveahi CHECK (viveahi IN ('S', 'N')),
+	tel varchar(20) not null,
+	sexo varchar(1)  NOT NULL 
+	CONSTRAINT CK_sexo CHECK (sexo IN ('F', 'M')),
+    fechnac datetime not null
+)
+
+
+Create table tipogasto	(
+	idtipogasto int primary key not null, 
+	descripcion varchar(50) not null
+)
+
+
+Create table consorcio	(
+	idprovincia int not null,
+    idlocalidad int not null,
+    idconsorcio int not null, 
+	nombre varchar(50) not null,
+	direccion varchar(250) not null,					     
+	idzona int not null,	
+	idconserje int not null,	
+	idadmin int not null,	
+	Constraint PK_consorcio PRIMARY KEY (idprovincia, idlocalidad,idconsorcio),
+	Constraint FK_consorcio_pcia FOREIGN KEY (idprovincia,idlocalidad)  REFERENCES localidad(idprovincia,idlocalidad),
+	Constraint FK_consorcio_zona FOREIGN KEY (idzona)  REFERENCES zona(idzona),						 					     					     					     				     					     
+	Constraint FK_consorcio_conserje FOREIGN KEY (idconserje)  REFERENCES conserje(idconserje),
+	Constraint FK_consorcio_admin FOREIGN KEY (idadmin)  REFERENCES administrador(idadmin)						 					     					     					     				     					     						 						 						 					     					     					     				     					     						 
+)
+
+Create table gasto (
+	idgasto int identity not null,
+	idprovincia int not null,
+    idlocalidad int not null,
+    idconsorcio int not null, 
+	periodo int not null,
+	fechapago datetime not null,					     
+	idtipogasto int not null,
+	importe decimal (8,2) not null,	
+	Constraint PK_gasto PRIMARY KEY (idgasto),
+	Constraint FK_gasto_consorcio FOREIGN KEY (idprovincia,idlocalidad,idconsorcio)  REFERENCES consorcio(idprovincia,idlocalidad,idconsorcio),
+	Constraint FK_gasto_tipo FOREIGN KEY (idtipogasto)  REFERENCES tipogasto(idtipogasto)					     					     						 					     					     
+)
+
+
+Create table gastonew (
+	idgasto int identity not null,
+	idprovincia int not null,
+    idlocalidad int not null,
+    idconsorcio int not null, 
+	periodo int not null,
+	fechapago datetime not null,					     
+	idtipogasto int not null,
+	importe decimal (8,2) not null,	
+	Constraint PK_gastonew PRIMARY KEY (idgasto),
+	Constraint FK_gastonew_consorcio FOREIGN KEY (idprovincia,idlocalidad,idconsorcio)  REFERENCES consorcio(idprovincia,idlocalidad,idconsorcio),
+	Constraint FK_gastonew_tipo FOREIGN KEY (idtipogasto)  REFERENCES tipogasto(idtipogasto)					     					     						 					     					     
+)
